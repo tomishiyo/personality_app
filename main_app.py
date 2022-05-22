4a0def1d142a2c57eb508eaa01c194ba676e41bc
tomishiyo <tomishiyo@gmail.com>
2022-05-22 17:22:10 -0300

    Added logout functionality. Started work in the 'Add' functionality. #TODO: Modify the ADD page to show only users that are not the logged in guest. #TODO: Integrate add with edit functionality #TODO: Add function to compute sum of points.

--- main_app.py
+++ main_app.py
@@ -1,7 +1,7 @@
 import sys
 
 from flask import Flask, render_template, request, url_for, redirect, flash
-from flask_login import UserMixin, LoginManager, login_user, current_user, login_required
+from flask_login import UserMixin, LoginManager, login_user, current_user, login_required, logout_user
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -19,18 +19,22 @@ login_manager.init_app(app)
 
 class User(UserMixin, user_db.Model):
     identifier = user_db.Column(user_db.Integer(), primary_key=True)
-    username = user_db.Column(user_db.String())
+    username = user_db.Column(user_db.String(), unique=True)
     name = user_db.Column(user_db.String())
     password = user_db.Column(user_db.String())
+    guesses = user_db.Column(user_db.PickleType())
+    character = user_db.Column(user_db.String())
+    alignment = user_db.Column(user_db.String())
+    circle = user_db.Column(user_db.String())
 
     def get_id(self):
            return self.identifier
 
 
-def add_example_user():
-    user = User(username='leonardocavalcante', password='coxinha123', name='Teti')
-    user_db.session.add(user)
-    user_db.session.commit()
+# def add_example_user():
+#     user = User(username='admin', password='admin', name='admin')
+#     user_db.session.add(user)
+#     user_db.session.commit()
 
 
 def read_answer_keys():
@@ -55,8 +59,6 @@ def read_answer_keys():
 def main():
     answer_keys = read_answer_keys()
     user_db.create_all()
-
-    # add_example_user()
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -86,7 +88,27 @@ def main():
     def profile_page():
         return render_template('profile.html', name=current_user.name)
 
-    app.run(host=('192.168.0.97'), debug=True)
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        return(redirect(url_for('index')))
+
+    @app.route('/add', methods=['GET', 'POST'])
+    @login_required
+    def add_guesses():
+        if request.method == 'GET':
+            return render_template('add.html')
+        else:
+            form = request.form
+            person = form['pessoas']
+            alignment = form['alignment']
+            character = form['character']
+            current_user.guesses = {person: {'alignment': alignment, 'character': character}}
+            user_db.session.commit()
+            return form
+
+    app.run(host='192.168.0.97', debug=True)
 
 
 if __name__ == '__main__':
